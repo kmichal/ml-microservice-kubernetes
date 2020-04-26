aa17f751a17613b4103d6fff3f1f57dcc0d4a54f
michal <mkrasucki@gmail.com>
2020-04-26 00:38:38 -0400

    add appropriate commands to the script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+repodomain=docker.io
+dockerrepo=michalkrasucki
+imagetag=api
+dockerpath=$dockerrepo/$imagetag
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $imagetag --image=$repodomain/$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/$imagetag 8000:80
