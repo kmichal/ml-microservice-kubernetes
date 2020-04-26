97ca3dec0dfeeb7a04e2b01c26037cf7496cbab6
michal <mkrasucki@gmail.com>
2020-04-25 21:21:27 -0400

    update docker upload script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,18 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerrepo=michalkrasucki
+imagetag=api
+
+dockerpath=$dockerrepo/$imagetag
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+docker image tag api $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
